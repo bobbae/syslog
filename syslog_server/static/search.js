9b30c6301b5ac79ae498a174020cc0d5385ce352
Bob Bae <bobbae@gmail.com>
2024-10-27 20:09:12 -0700

    added templates and static

---
+++ syslog_server/static/search.js
@@ -0,0 +1,28 @@
+function searchTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("syslog-table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        var display = false;
+        for (var j = 0; j < td.length; j++) {
+            if (td[j]) {
+                txtValue = td[j].textContent || td[j].innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    display = true;
+                    break;
+                }
+            }
+        }
+        tr[i].style.display = display ? "" : "none";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('htmx:afterSwap', function(event) {
+        searchTable();
+    });
+});
